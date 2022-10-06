59ae33cb645001b9cb66115ba7ddb1a5711d04d1
Alex Borel <borelalex0@gmail.com>
2022-10-06 17:16:05 -0400

    commit tree 19374aa476ee363be370acf14b82486e5856f7c9

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    return gifts;
+}
+
+function writeCards(array, event) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+    return n;
+}
